545cdca7ad3652b88d3fa58f0aacbd16d64efd29
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-20 14:25:04 +0100

    feat: create factory for account-validator

---
+++ src/account/factory/account-validator-factory.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-extraneous-class */
+import ValidatorInterface from '../../@shared/validators/validator-interface'
+import Account from '../entity/account-entity'
+import AccountYupValidator from '../validator/account-yup-validator'
+
+export default class AccountValidatorFactory {
+  static create (): ValidatorInterface<Account> {
+    return new AccountYupValidator()
+  }
+}
