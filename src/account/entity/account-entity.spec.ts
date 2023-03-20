39a141163d9b119c68beaad9a38fbda2c3b7ea47
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-20 14:56:02 +0100

    test: ensure account throws if an invalid email is passed

--- src/account/entity/account-entity.spec.ts
+++ src/account/entity/account-entity.spec.ts
@@ -14,4 +14,18 @@ describe('AccountEntity test', () => {
       password: ''
     })).toThrow()
   })
+
+  test('Should throw if an invalid email address is provided', () => {
+    expect(() => new Account({
+      name: 'any_name',
+      burth: new Date(),
+      country: 'any_country',
+      city: 'any_city',
+      address: 'any_address',
+      postalCode: 'any_post',
+      phone: 'any_phone',
+      email: 'any_email',
+      password: 'any_password'
+    })).toThrow()
+  })
 })
