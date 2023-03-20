b608653123813d35e0e73b3459af973ad0c692e7
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-20 11:08:50 +0100

    feat: add dropDb method on mongodb-memory-server utils

--- src/shared/utils/mongodb-memory-server.ts
+++ src/shared/utils/mongodb-memory-server.ts
@@ -3,11 +3,19 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 
 let mongo: MongoMemoryServer | null = null
 
-export const connectDB = async (): Promise<void> => {
+export const connectDb = async (): Promise<void> => {
   mongo = await MongoMemoryServer.create()
   const uri = mongo.getUri()
 
   await mongoose.connect(uri, {
     dbName: 'finapi'
   })
+}
+
+export const dropDb = async (): Promise<void> => {
+  if (mongo) {
+    await mongoose.connection.dropDatabase()
+    await mongoose.connection.close()
+    await mongo.stop()
+  }
 }
