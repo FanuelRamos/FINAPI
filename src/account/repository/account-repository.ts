96e8ae45e7155a6eda1e1392402a3b24c05a854c
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-21 15:06:58 +0100

    feat: implement findById method on AccountRepository

--- src/account/repository/account-repository.ts
+++ src/account/repository/account-repository.ts
@@ -1,10 +1,11 @@
 import { FilterQuery } from 'mongoose'
-import AccountEntity from '../entity/account-entity'
+import Account from '../entity/account-entity'
 import AccountGateway from '../gateway/account-gateway'
 import { AccountModel } from './account-model'
+import Id from '../../@shared/domain/value-object/id-value-object'
 
 export default class AccountRepository implements AccountGateway {
-  async add (account: AccountEntity): Promise<void> {
+  async add (account: Account): Promise<void> {
     await AccountModel.create({
       id: account.id,
       name: account.name,
@@ -20,15 +21,29 @@ export default class AccountRepository implements AccountGateway {
     })
   }
 
-  async find (filter: FilterQuery<unknown>): Promise<AccountEntity | null> {
+  async findById (id: string): Promise<Account | null> {
+    const account = await AccountModel.findOne({ id })
+    if (!account) return null
+    return new Account({
+      id: new Id(account.id),
+      name: account.name!,
+      burth: account.burth!,
+      country: account.country!,
+      city: account.city!,
+      address: account.address!,
+      postalCode: account.postalCode!,
+      phone: account.phone!,
+      email: account.email!,
+      createdAt: account.createdAt,
+      updatedAt: account.updatedAt
+    })
+  }
+
+  async find (filter: FilterQuery<unknown>): Promise<Account | null> {
     throw new Error('Method not implemented.')
   }
 
-  async findById (id: string): Promise<AccountEntity | null> {
-    throw new Error('Method not implemented.')
-  }
-
-  async update (account: AccountEntity): Promise<AccountEntity | null> {
+  async update (account: Account): Promise<Account | null> {
     throw new Error('Method not implemented.')
   }
 }
