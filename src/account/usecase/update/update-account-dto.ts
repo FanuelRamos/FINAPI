b10f9d78e202b7e2f4d6204fd2390aa07a425cee
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-23 13:57:09 +0100

    refactor: add id property to the UpdateAccountUseCaseInputDTO and make the orther property as optional

--- src/account/usecase/update/update-account-dto.ts
+++ src/account/usecase/update/update-account-dto.ts
@@ -1,12 +1,13 @@
 export interface UpdateAccountUseCaseInputDTO {
-  name: string
-  burth: Date
-  country: string
-  city: string
-  address: string
-  postalCode: string
-  phone: string
-  email: string
+  id: string
+  name?: string
+  burth?: Date
+  country?: string
+  city?: string
+  address?: string
+  postalCode?: string
+  phone?: string
+  email?: string
 }
 
 export interface UpdateAccountUseCaseOutputDTO {
