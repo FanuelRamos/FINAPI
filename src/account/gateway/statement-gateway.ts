a656c5787426f7559876bfd48432df24c9a6d022
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-23 17:15:58 +0100

    feat: define a input interface on gateway for Statement

--- src/account/gateway/statement-gateway.ts
+++ src/account/gateway/statement-gateway.ts
@@ -1,7 +1,14 @@
 import { FilterQuery } from 'mongoose'
 import Statement from '../../@shared/domain/value-object/statement-value-object'
 
-export default interface AccountGateway<T> {
-  addStatement(input: T): Promise<Statement>
+export interface AddStatementUseCaseInputDTO {
+  account: string
+  transaction: string
+  amount: number
+  type: string
+}
+
+export default interface AccountGateway {
+  addStatement(input: AddStatementUseCaseInputDTO): Promise<Statement>
   findStatement(filter: FilterQuery<unknown>): Promise<Statement[] | null>
 }
