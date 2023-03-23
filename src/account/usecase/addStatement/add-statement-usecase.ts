4470dc8a50d53093e448fe2434096a31490ea328
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-23 19:09:53 +0100

    feat: ensure AddStatementUseCase return a statement on success

--- src/account/usecase/addStatement/add-statement-usecase.ts
+++ src/account/usecase/addStatement/add-statement-usecase.ts
@@ -5,10 +5,16 @@ import { AddStatementUseCaseInputDTO, AddStatementUseCaseOutputDTO } from './add
 export default class AddStatementUseCase implements UseCaseInterface<AddStatementUseCaseInputDTO, AddStatementUseCaseOutputDTO> {
   constructor (private _repository: StatementGateway) {}
 
-  async execute (input: AddStatementUseCaseInputDTO): Promise<any> {
+  async execute (input: AddStatementUseCaseInputDTO): Promise<AddStatementUseCaseOutputDTO> {
     const statement = await this._repository.addStatement(input)
     if (!statement) {
       throw new Error('Could not add statement')
+    }
+
+    return {
+      transaction: statement.transaction.id,
+      amount: statement.amount,
+      type: statement.type
     }
   }
 }
