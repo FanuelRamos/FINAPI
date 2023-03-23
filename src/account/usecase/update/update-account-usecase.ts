6ad1997454c44de6a026bd592e4ebc50af84936b
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-23 14:13:41 +0100

    feat: ensure UpdateAccountUseCase throws if the account could not be updated

--- src/account/usecase/update/update-account-usecase.ts
+++ src/account/usecase/update/update-account-usecase.ts
@@ -10,5 +10,19 @@ export default class UpdateAccountUseCase implements UseCaseInterface<UpdateAcco
     if (!account) {
       throw new Error('Account not found')
     }
+
+    account.name = input.name ?? account.name
+    account.burth = input.burth ?? account.burth
+    account.country = input.country ?? account.country
+    account.city = input.city ?? account.city
+    account.address = input.address ?? account.address
+    account.postalCode = input.postalCode ?? account.postalCode
+    account.phone = input.phone ?? account.phone
+    account.email = input.email ?? account.email
+
+    const updatedAccount = await this._repository.update(account)
+    if (!updatedAccount) {
+      throw new Error('Could not update account')
+    }
   }
 }
