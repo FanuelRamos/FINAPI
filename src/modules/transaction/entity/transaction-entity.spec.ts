7211da81c4bcaf1d970e4b2a1a747415db96793e
Fanuel Ramos <fanuelramos111@gmail.com>
2023-05-08 22:36:16 +0100

    test: ensure that is necessary an amount of 100 to create a Transaction

--- src/modules/transaction/entity/transaction-entity.spec.ts
+++ src/modules/transaction/entity/transaction-entity.spec.ts
@@ -1,3 +1,4 @@
+import Id from '../../@shared/domain/value-object/id-value-object'
 import Transaction from './transaction-entity'
 
 describe('Transactions tests', () => {
@@ -8,6 +9,16 @@ describe('Transactions tests', () => {
       recipientAccount: '',
       recepientName: '',
       amount: 100
+    })).toThrow()
+  })
+
+  test('Should throw if amount is less than 100', () => {
+    expect(() => new Transaction({
+      senderAccount: new Id().id,
+      senderName: 'Any_Sender_Name',
+      recipientAccount: new Id().id,
+      recepientName: 'Any_Recepient_Name',
+      amount: 90
     })).toThrow()
   })
 })
