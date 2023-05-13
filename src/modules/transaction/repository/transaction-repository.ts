f16e911eabff6ec8fcd83a0028e1a90bf7f36ecb
Fanuel Ramos <fanuelramos111@gmail.com>
2023-05-13 11:03:46 +0100

    feat: implement findByID method

--- src/modules/transaction/repository/transaction-repository.ts
+++ src/modules/transaction/repository/transaction-repository.ts
@@ -2,6 +2,7 @@ import { FilterQuery } from 'mongoose'
 import TransactionEntity from '../entity/transaction-entity'
 import TransactionGateway from '../gateway/transaction-gateway'
 import { TransactionModel } from './transaction-model'
+import Id from '../../@shared/domain/value-object/id-value-object'
 
 export default class TransactionRepository implements TransactionGateway {
   async add (transaction: TransactionEntity): Promise<void> {
@@ -17,7 +18,22 @@ export default class TransactionRepository implements TransactionGateway {
     })
   }
 
-  async find (filter: FilterQuery<unknown>): Promise<TransactionEntity> {
-    throw new Error('Method not implemented.')
+  async findById (id: string): Promise<TransactionEntity | null> {
+    const transaction = await TransactionModel.findOne({ id })
+    if (!transaction) return null
+    return new TransactionEntity({
+      id: new Id(transaction.id),
+      senderAccount: transaction.senderAccount!,
+      senderName: transaction.senderName!,
+      recipientAccount: transaction.recipientAccount!,
+      recipientName: transaction.recipientName!,
+      amount: transaction.amount!,
+      createdAt: transaction.createdAt,
+      updatedAt: transaction.updatedAt
+    })
+  }
+
+  async find (filter: FilterQuery<unknown>): Promise<TransactionEntity | null> {
+    throw new Error('Not implemented')
   }
 }
