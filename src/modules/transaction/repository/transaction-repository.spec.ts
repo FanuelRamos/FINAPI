0706bc7a8d2c0f7f6d3721d929d00d4df8206779
Fanuel Ramos <fanuelramos111@gmail.com>
2023-05-13 11:22:22 +0100

    test: ensure find return a transaction exists a transaction with given filters

--- src/modules/transaction/repository/transaction-repository.spec.ts
+++ src/modules/transaction/repository/transaction-repository.spec.ts
@@ -80,6 +80,32 @@ describe('TransactionRepository tests', () => {
     expect(result).toBeFalsy()
   })
 
+  test('Should return a transaction if exists a transaction with given filters', async () => {
+    const transactionRepository = makeSut()
+    await TransactionModel.create({
+      id: fakeTransaction.id.id,
+      senderAccount: fakeTransaction.senderAccount,
+      senderName: fakeTransaction.senderName,
+      recipientAccount: fakeTransaction.recipientAccount,
+      recipientName: fakeTransaction.recipientName,
+      amount: fakeTransaction.amount,
+      createdAt: fakeTransaction.createdAt,
+      updatedAt: fakeTransaction.updatedAt
+    })
+
+    const result = await transactionRepository.find({ senderName: fakeTransaction.senderName })
+
+    expect(result).toBeTruthy()
+    expect(result?.id).toBeDefined()
+    expect(result?.senderAccount).toBe(fakeTransaction.senderAccount)
+    expect(result?.senderName).toBe(fakeTransaction.senderName)
+    expect(result?.recipientAccount).toBe(fakeTransaction.recipientAccount)
+    expect(result?.recipientName).toBe(fakeTransaction.recipientName)
+    expect(result?.amount).toBe(fakeTransaction.amount)
+    expect(result?.createdAt).toEqual(fakeTransaction.createdAt)
+    expect(result?.updatedAt).toEqual(fakeTransaction.updatedAt)
+  })
+
   afterEach(async () => {
     await dropCollections()
   })
