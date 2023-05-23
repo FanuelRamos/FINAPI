a95199f9c9ee9ffd8e75fa021268807c3eea1b9d
Fanuel Ramos <fanuelramos111@gmail.com>
2023-05-23 18:05:41 +0100

    feat: ensure POST /transaction returns 200 on success

---
+++ src/main/routes/transaction.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import TransactionFacadeFactory from '../../modules/transaction/factory/transaction-facade-factory'
+
+export default (router: Router): void => {
+  router.post('/transaction', adaptRoute(TransactionFacadeFactory, 'add'))
+}
