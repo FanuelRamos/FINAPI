7ff9d9b48ce60ad0119f621b4be292a45e1f81df
Fanuel Ramos <fanuelramos111@gmail.com>
2023-05-25 01:38:10 +0100

    feat: ensure AddStatement do not add a debit statement with no founds

--- src/modules/account/usecase/addStatement/add-statement-usecase.ts
+++ src/modules/account/usecase/addStatement/add-statement-usecase.ts
@@ -1,11 +1,22 @@
+/* eslint-disable prefer-regex-literals */
 import UseCaseInterface from '../../../@shared/usecase/usecase-interface'
 import AccountGateway from '../../gateway/account-gateway'
+import GetBalanceUseCase from '../get-balance/get-balance-usecase'
 import { AddStatementUseCaseInputDTO, AddStatementUseCaseOutputDTO } from './add-statement-dto'
 
 export default class AddStatementUseCase implements UseCaseInterface<AddStatementUseCaseInputDTO, AddStatementUseCaseOutputDTO> {
-  constructor (private _repository: AccountGateway) {}
+  private _getBalance: GetBalanceUseCase
+  constructor (private _repository: AccountGateway) {
+    this._getBalance = new GetBalanceUseCase(this._repository)
+  }
 
   async execute (input: AddStatementUseCaseInputDTO): Promise<AddStatementUseCaseOutputDTO> {
+    if (input.type === 'debit') {
+      const balance = await (await this._getBalance.execute({ id: input.account }))?.balance
+      if (balance < input.amount) {
+        throw new Error('Insufficient funds')
+      }
+    }
     const statement = await this._repository.addStatement(input)
     if (!statement) {
       throw new Error('Could not add statement')
